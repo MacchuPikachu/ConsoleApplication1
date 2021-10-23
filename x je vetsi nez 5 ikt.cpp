f1771c8554816fb91151d5518eaa90bc9773c387
MacchuPikachu <92727482+MacchuPikachu@users.noreply.github.com>
2021-10-23 19:53:32 +0200

    Add files via upload

---
+++ x je vetsi nez 5 ikt.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication1.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int x = 6;
+	printf("%d ", x);
+	if (x > 5) {
+		;
+		printf("je vetsi nez 5");
+	}
+	else {
+		printf("neni mensi nez 5");
+	}
+}
